764ff83c08cf43ce9b54dd04ae1fba2d7f0e5f56
Olusegun Adeyemi <segunisaboy@gmail.com>
2020-03-21 10:44:57 +0100

    Second trial

---
+++ precache-manifest.db4bb63e64eba46061ba9d1d7e83ec90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1abb00bd4bc4a221b70e09e56baa4824",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3f73a11956c2b514594",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0e3259158b2bdb80c0ad",
+    "url": "/static/css/main.6adb1e6f.chunk.css"
+  },
+  {
+    "revision": "f3f73a11956c2b514594",
+    "url": "/static/js/2.970f04b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.970f04b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e3259158b2bdb80c0ad",
+    "url": "/static/js/main.b81e1794.chunk.js"
+  },
+  {
+    "revision": "fc7085ebf2b1685c2d0e",
+    "url": "/static/js/runtime-main.f3f690bd.js"
+  }
+]);
